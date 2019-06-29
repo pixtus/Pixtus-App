5b7c2402303cfef5326ecc72845c7342a0674261
yjh5424 <yjh54240@naver.com>
2019-06-30 03:25:33 +0900

    top ui merge

--- PixtusApp/app/src/main/java/com/mash/up/pixtus_app/ui/MainActivity.kt
+++ PixtusApp/app/src/main/java/com/mash/up/pixtus_app/ui/MainActivity.kt
@@ -3,9 +3,11 @@ package com.mash.up.pixtus_app.ui
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
+import com.bumptech.glide.Glide
 import com.google.gson.annotations.SerializedName
 import com.mash.up.pixtus_app.R
 import com.mash.up.pixtus_app.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -21,6 +23,8 @@ class MainActivity : BaseActivity() {
 
 
     fun initUI(){
+        Glide.with(this).asGif().load(R.raw.pixel_best).into(iv_gif)
+
         var dateFormat = SimpleDateFormat("MM.dd / EEE")
         tv_date.text = dateFormat.format(Date()).toString()
     }
