90ff03a3ed2ee7df0a8d3104f79bf77d73324c70
SeongYunKim <cauboy17@naver.com>
2019-07-13 02:00:40 +0900

    Modify ImageView Size

--- PixtusApp/app/src/main/java/com/mash/up/pixtus_app/ui/StoryActivity.kt
+++ PixtusApp/app/src/main/java/com/mash/up/pixtus_app/ui/StoryActivity.kt
@@ -13,7 +13,8 @@ import kotlinx.android.synthetic.main.activity_story.*
 
 class StoryActivity : BaseActivity() {
 
-    var story_string: Array<String> = arrayOf("픽터스는 꽃을 피우기 위해 무던히 노력했지만\n혼자서는 쉽지 않았답니다.",
+    var story_string: Array<String> = arrayOf("픽터스는 꽃을 피우기 위해 무던히 노력했지만\n...",
+        "픽터스는 꽃을 피우기 위해 무던히 노력했지만\n혼자서는 쉽지 않았답니다.",
         "외로운 픽터스 에게는 함께 노력해줄 친구가 필요했어요.", "\"어이, 거기!!\"", "\"그래, 거기 스마트폰 밖에 있는 당신\"",
         "\"나와 함께 열심히 꽃 피워 보겠어?\"")
     var current_step = -1
@@ -26,22 +27,22 @@ class StoryActivity : BaseActivity() {
 
         story_next_btn.setOnClickListener {
             current_step++
-            if(current_step < 5) {
+            if(current_step < 6) {
                 tv_story.text = story_string[current_step]
                 when(current_step){
                     0->Glide.with(this).asGif().load(R.raw.pixtus_story02).into(imageView3)
-                    1->Glide.with(this).asGif().load(R.raw.pixtus_story03).into(imageView3)
-                    2->{
+                    2->Glide.with(this).asGif().load(R.raw.pixtus_story03).into(imageView3)
+                    3->{
                         tv_story.setTextColor(Color.parseColor("#5dbb96"))
                         Glide.with(this).asGif().load(R.raw.pixtus_story04).into(imageView3)
-                    }
-                    4->{
                         tv_narration.text = "PIXTUS"
+                    }
+                    5->{
                         tv_together.setTextColor(Color.parseColor("#5dbb96"))
                     }
                 }
             }
-            else if (current_step == 5) {
+            else if (current_step == 6) {
                 moveNextPage()
             }
         }
@@ -52,11 +53,11 @@ class StoryActivity : BaseActivity() {
             tv_story.setTextColor(Color.parseColor("#5dbb96"))
             tv_story.text = story_string[4]
             Glide.with(this).asGif().load(R.raw.pixtus_story04).into(imageView3)
-            current_step = 4
+            current_step = 5
         }
 
         tv_together.setOnClickListener() {
-            if(current_step == 4)
+            if(current_step == 5)
                 moveNextPage()
         }
     }
