2b1abbfb302b7c9757459eb4fab3cb71aa387e8e
Tak Hyungmin <milkho11@naver.com>
2019-07-17 00:50:42 +0900

    add HistoryApi.kt

---
+++ PixtusApp/app/src/main/java/com/mash/up/pixtus_app/core/HistoryApi.kt
@@ -0,0 +1,13 @@
+package com.mash.up.pixtus_app.core
+
+import io.reactivex.Single
+import retrofit2.http.GET
+
+
+interface HistoryApi {
+
+//    @GET("/workout/history") // 리턴타입
+//    fun getWorkoutHistory() : Single<List<>>
+
+}
+
