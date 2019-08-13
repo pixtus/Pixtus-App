cff704bfef66d72491589a4f7e438499bb2e3071
Tak Hyungmin <milkho11@naver.com>
2019-08-13 21:56:45 +0900

    Add HistoryAPI

--- PixtusApp/app/src/main/java/com/mash/up/pixtus_app/core/HistoryApi.kt
+++ PixtusApp/app/src/main/java/com/mash/up/pixtus_app/core/HistoryApi.kt
@@ -1,13 +1,22 @@
 package com.mash.up.pixtus_app.core
 
+import com.mash.up.pixtus_app.ui.history.data.HistoryResponse
 import io.reactivex.Single
 import retrofit2.http.GET
-
+import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface HistoryApi {
 
-//    @GET("/workout/history") // 리턴타입
-//    fun getWorkoutHistory() : Single<List<>>
+    @GET("/history")
+    fun getHistory(
+        @Header("Authorization") authorization: String,
+//        // history/{prevWeek}
+//        @Path ("prevWeek") prevWeek: Int?,
+
+        // history/?prevWeek=2
+        @Query("prevWeek") prevWeek: Int?
+    ): Single<HistoryResponse>
 
 }
 
